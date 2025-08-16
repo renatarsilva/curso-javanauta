9b975b1c93f490a45e3d0e6c108c9bf8d7a1987f
Renata <renata.rodriguesdasilva@outlook.com>
2025-08-16 18:57:39 -0300

    feat: update

---
+++ src/estruturaFor.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class estruturaFor {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+
+        for (int contador =0; contador<5; contador++) {
+            System.out.println("contador do For" + contador);
+        }
+    }
+}
